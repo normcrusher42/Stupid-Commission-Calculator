e46cd259c2a150306548a74b60273e33268be9c7
Nasser Nasser <nanasser@lab1r3s6.42abudhabi.ae>
2025-03-18 02:17:16 +0400

    fini

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Stupid-Commission-Calculator
+
+	███████╗ ██████╗ ██████╗ 
+	██╔════╝██╔════╝██╔════╝ 
+	███████╗██║     ██║      
+	╚════██║██║     ██║      
+	███████║╚██████╗╚██████╗ 
+	╚══════╝ ╚═════╝ ╚═════╝ 
+     𝕊𝕥𝕦𝕡𝕚𝕕 ℂ𝕠𝕞𝕞𝕚𝕤𝕤𝕚𝕠𝕟 ℂ𝕒𝕝𝕔𝕦𝕝𝕒𝕥𝕠𝕣
+
+a stupid math program made to lazily calculate my overall income from commissions
+(along with other small factors like fees n conversions) so I don't need to use notepad && calculator ever again.
+
+𝐇𝐨𝐰 𝐢𝐭 𝐰𝐨𝐫𝐤𝐬:
+Litorlee just a bunch of printfs with math in its parameter when needed. I was expecting this to be more complicated but turns out
+I still am a dummy at code :D
+
+There're probably better ways to do this but with what I can do now, this is an easy solution and I don't intend on designing this 
+for public use as it's probably useless to many but myself. Hoping I get to improve it with time 🤞
+
+How to run:
+
+```cc *.c | ./a.out```
+
+yea just compile and run it ik its crazy bro believe me it's not arch-linux science
--- stupid_calculator.c
+++ stupid_calculator.c
@@ -8,7 +8,7 @@ void	stupid_calculator(void)
 	// number of times I payed the withdrawel fee
 	int		withdraw = 2;
 	// Celery
-	int		comm1 = 15 + 7;
+	int		comm1 = 15 + 7; // tipped me $7 ^-^
 	float	comm1PP = 14.04 + 6.39;
 	// Wolfguy I forgor
 	int		comm2 = 10 + 20;
@@ -20,10 +20,10 @@ void	stupid_calculator(void)
 	int		comm4 = 150 + 25;
 	float	comm4PP = 143.10 + 23.60;
 	// Wwames
-	int		comm5 = 15;
+	int		comm5 = 15; // tipped me $3 ^-^
 	float	comm5PP = 14.04;
 	// Flame
-	int		comm6 = 30 + 10;
+	int		comm6 = 30 + 10; // tipped me $5 ^-^
 	float	comm6PP = 9.26 + 28.38;
 	// Bein
 	int		comm7 = 80;
@@ -32,10 +32,25 @@ void	stupid_calculator(void)
 	float	sum = (comm1 + comm2 + comm3 + comm4 + comm5 + comm6 + comm7); // og sum
 	float	sumPP = (comm1PP + comm2PP + comm3PP + comm4PP + comm5PP + comm6PP + comm7PP); // paypal sum from fees
 
+	// Start with Client count
 	printf("\nTotal num of clients as of now : %d", lsclients);
-	// Start with original prices
-	printf("\nOG sum : $%d + $%d + $%.2f + $%d + $%d + $%d + $%d = $%.2f", comm1, comm2, comm3, comm4, comm5, comm6, comm7, sum);
+	// Then OG sum
+	printf("\nOG sum :      $%d    + $%d    + $%.2f + $%d    + $%d    + $%d    + $%d    = $%.2f", comm1, comm2, comm3, comm4, comm5, comm6, comm7, sum);
+	// Then PP fees sum
 	printf("\nPP fees sum : $%.2f + $%.2f + $%.2f + $%.2f + $%.2f + $%.2f + $%.2f = $%.2f", comm1PP, comm2PP, comm3PP, comm4PP, comm5PP, comm6PP, comm7PP, sumPP);
-	printf("\nI'd gotten $%.2f but paypal's shitty fees reducted it by -$%.2f\nSo that's a %.2f%% loss :/ (-$%.2f (%.2f%%) if I include withdrawel fees)", sum, (sum - sumPP), ((sumPP / sum) * 100), (withdraw * 3 + (sum - sumPP)), (withdraw * 3 + ((sumPP / sum) * 100)));
-	printf("\n")
+	// Elaborate
+	printf("\nI'd gotten $%.2f but paypal's shitty fees reducted it by -$%.2f. So that's a %.2f%% loss :/ (-$%.2f (%.2f%%) if I include withdrawal fees)", sum, (sum - sumPP), (((sum - sumPP) / sum) * 100), ((sum - sumPP) + (withdraw * 3)), (((withdraw * 3) + (sum - sumPP)) / sum) * 100);
+
+	// Overall summary
+	printf("\n\nTo summarize: ");
+	printf("\nNum of Clients : %d", lsclients);
+	printf("\nOG :     $%.2f\nPP fee : $%.2f ($%.2f precisely)", sum, sumPP, (sumPP - (withdraw * 3)));
+	printf("\nCelery, Wwames, & Flame had tipped me ^v^ ($15 total)");
+	printf("\nTLDR; fuck Paypal. Got no other options.");
+
+	printf("\n\nOG currency :     AED%.2f", sum * 3.67);
+	printf("\nPP fee currency : AED%.2f", sumPP * 3.67);
+	printf("\nAmount Lost :    -AED%.2f\n", (sum - sumPP) * 3.67);
+
+	// if I wanna add a new variable I'll only have to addnDeclare a new variable, add another float specifier and paramater variable. I hope future me doesn't forget :v
 }
