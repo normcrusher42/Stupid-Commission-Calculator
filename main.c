95235c4bcab06be80c97af69dba7e27bdf1258e4
Nasser Nasser <nanasser@lab1r3s6.42abudhabi.ae>
2025-03-18 00:20:08 +0400

    nvm my printf doesn't handle floats RIP

--- main.c
+++ main.c
@@ -10,5 +10,24 @@
 /* 
 a stupid program math program made to lazily calculate my overall income from commissions
 (along with other small factors like fees n conversions) so I don't need to use notepad && calculator ever again.
+
+
+𝐇𝐨𝐰 𝐢𝐭 𝐰𝐨𝐫𝐤𝐬: 
+It's gonna run similarly to printf (probably gonna reuse n tweak my printf function) except it won't need type specifiers 
+nor Variadic Functions as the whole point is to NOT rewrite the numbers again but instead append new variables to existing ones
+(meaning I'd have to edit the code a bit featuring a new variable). 
+
+There're probably better ways to do this but with what I can do now, this is an easy solution and I don't intend on designing this 
+for public use as it's probably useless to many but myself (or anyone alike).
+
+edit : NVM my function doesn't handle float pointers so screw that
+𝕋𝕙𝕖 𝕡𝕝𝕒𝕟:
+void function with void parameter cause we returning nothin, 
 */
 
+
+
+int	main(void)
+{
+
+}
---
+++ stupid_calculator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void	stupid_calculator(void)
+{
+	int		comm1 = 15 + 7; // Celery
+	float	comm1PP = 14.04 + 6.39;
+
+	int		comm2 = 10 + 20; // 
+	float	comm2PP = 9.26 + 18.82;
+	float	comm3 = 244.59 + 242.06;
+	float	comm3PP = 233.53 + 231.11;
+	int		comm4 = 150 + 25;
+	float	comm4PP = 143.10 + 23.60;
+	int		comm5 = 15;
+	float	comm5PP = 14.04;
+	int		comm6 
+}
